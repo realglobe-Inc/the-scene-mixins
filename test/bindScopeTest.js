afc21775838def246a5d5e8a10ac1f4a8c8d8dcb
okunishinishi <okunishinishi@gmail.com>
2018-01-27 16:30:17 +0900

    Releasing new package

--- test/bindScopeTest.js
+++ test/bindScopeTest.js
@@ -5,7 +5,7 @@
 'use strict'
 
 const bindScope = require('../lib/bindScope')
-const { ok, equal } = require('assert')
+const {ok, equal} = require('assert')
 
 describe('bindScope', () => {
   before(() => {
@@ -15,7 +15,19 @@ describe('bindScope', () => {
   })
 
   it('Do test', () => {
-
+    const Bound = bindScope('foo.bar')(
+      class {
+        get store () {
+          return {
+            foo: {
+              bar: {m: 'This is bar'}
+            }
+          }
+        }
+      }
+    )
+    const bound = new Bound()
+    equal(bound.scope.m, 'This is bar')
   })
 })
 
