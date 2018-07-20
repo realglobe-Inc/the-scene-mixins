734c6eaf5bdeacdbeea3859910be433143b8f9e5
okunishinishi <okunishinishi@gmail.com>
2018-07-20 15:37:30 +0900

    Releasing new package

---
+++ test/withFormTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for withForm.
+ * Runs with mocha.
+ */
+'use strict'
+
+const withForm = require('../lib/withForm')
+const { ok, equal } = require('assert')
+
+describe('with-form', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
