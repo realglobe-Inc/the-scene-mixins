e1b7e12f076446b8ea1a8f204353e29b6edf70cf
okunishinishi <okunishinishi@gmail.com>
2018-08-28 15:19:31 +0900

    Releasing new package

---
+++ test/withResizeTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for withResize.
+ * Runs with mocha.
+ */
+'use strict'
+
+const withResize = require('../lib/withResize')
+const { ok, equal } = require('assert')
+
+describe('with-resize', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
